295d85fdde5c07d124d0b919ad686f8034f53db5
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-09 19:39:31 -0300

    feat: :sparkles: Add routes to express

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const toDoListRouter = require('./src/routes/toDoList');
 const app = express();
+
+app.use('/todo', toDoListRouter)
 
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000.')
