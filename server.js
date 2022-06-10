c0780a2006ec1b7346b7f42f8fd30972d64a20ef
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-09 21:55:51 -0300

    feat: :construction: Add urlencoded and json - express

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const toDoListRouter = require('./src/routes/notes');
 const app = express();
 require('./config/database');
 
-app.use('/', toDoListRouter)
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use('/', toDoListRouter);
 
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000.')
