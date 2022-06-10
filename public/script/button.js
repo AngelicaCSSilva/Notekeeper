92dd34b847ee4264191cd1322d2615f3a95340af
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-10 19:02:17 -0300

    fix: :bug: Fix stateHandle

--- public/script/button.js
+++ public/script/button.js
@@ -6,7 +6,7 @@ title.addEventListener("change", stateHandle);
 note.addEventListener("change", stateHandle);
 
 function stateHandle() {
-    if (title.value.length > 0 && note.innerHTML.length > 0) {
+    if (title.value.length > 0 && note.value.length > 0) {
         button.disabled = false;
     } else {
         button.disabled = true;
