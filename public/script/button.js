4e1a6ced76d9414769c05ee9a384b51e4f6d0016
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-10 18:40:48 -0300

    feat: :construction: Add script with button state change

---
+++ public/script/button.js
@@ -0,0 +1,14 @@
+let button = document.querySelector("#button");
+let title = document.querySelector("#title-input");
+let note = document.querySelector("#note-input");
+
+title.addEventListener("change", stateHandle);
+note.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if (title.value.length > 0 && note.innerHTML.length > 0) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}
