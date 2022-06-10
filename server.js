76ed0ff307026e010002c168e7be56a34df84ee5
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-10 10:08:19 -0300

    feat: :construction: Add view engine EJS

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const toDoListRouter = require('./src/routes/notes');
+const path = require ('path');
+
 const app = express();
 require('./config/database');
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
 app.use('/', toDoListRouter);
 
 app.listen(3000, () => {
