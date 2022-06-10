9b0cc0efb989a78cdfb70a701d98c4cee7b73386
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-10 10:08:50 -0300

    feat: :construction: Add static path

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ require('./config/database');
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
